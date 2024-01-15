82bd018ad39b8de641fc1993b273e36ffea96889
Sarvadnya Jawale <127713222+sarvadnyaJawale@users.noreply.github.com>
2024-01-16 02:33:07 +0530

    Update main.tf

--- Monirtor/Terraform/main.tf
+++ Monirtor/Terraform/main.tf
@@ -87,7 +87,7 @@ resource "aws_eks_node_group" "example" {
 
   scaling_config {
     desired_size = 1
-    max_size     = 2
+    max_size     = 5
     min_size     = 1
   }
   instance_types = ["t2.medium"]
